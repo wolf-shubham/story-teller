a72e26188534aea0f2aa337eb30396a4fa9cb082
Shubham Sharma <sharma.shubham0191@gmail.com>
2022-03-15 14:43:29 +0530

    frontend initialized

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,21 @@
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home';
+import Register from './pages/Register';
+import Login from './pages/Login';
+import Header from './components/Header';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h2>story teller...</h2>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path='/' exact element={<Home />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/login' element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App;
